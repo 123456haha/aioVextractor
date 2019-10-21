7e88ea9951d612857c7cc6e00026687ba3d222b8
Panos <panoslin@foxmail.com>
2019-10-21 10:48:53 +0800

    optimize demo-api

--- __init__.py
+++ __init__.py
@@ -9,6 +9,8 @@ import sys, os
 curPath = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(curPath)
 
+from aioVextractor.distributor import distribute
+
 
 # from aioVextractor import (
 #     breaker,
--- demo-api.py
+++ demo-api.py
@@ -10,16 +10,17 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
-
 from sanic import response as Response
 from sanic import Sanic
 from sanic_cors import CORS
 import platform
-from aioVextractor.extractor.pinterest import Extractor
+from aioVextractor.extractor import *
+from aioVextractor import distribute
 import aiohttp
 
 if platform.system() in {"Linux", "Darwin"}:
     import uvloop
+
     uvloop.install()
 else:
     pass
@@ -42,16 +43,25 @@ async def extractor(request):
             url = request.form.get('url')
     if url:
         print(f"url: {url}")
-        with Extractor() as dinosaur:
-            async with  aiohttp.ClientSession() as session:
-                result = await dinosaur.entrance(webpage_url=url, session=session)
-                print(f"result: {result}")
-                print(f"result[0]['play_addr']: {result[0]['play_addr']}")
-                return Response.text(body=result[0]['play_addr'])
+        distribute_result = distribute(webpage_url=url)
+        if isinstance(distribute_result, str):
+            return Response.json({"msg": distribute_result,
+                                  "data": None},
+                                 status=400)
+        else:
+            info_extractor = distribute_result
+            with info_extractor() as dinosaur:
+                async with  aiohttp.ClientSession() as session:
+                    result = await dinosaur.entrance(webpage_url=url, session=session)
+                    # return Response.text(body=result[0]['play_addr'])
+                    return Response.json({
+                        "msg": "success",
+                        "data": result
+                    })
     else:
-        return Response.json({"code": 400,
-                              "msg": "There is not enough input🤯",
-                              "data": None})
+        return Response.json({"msg": "There is not enough input🤯",
+                              "data": None},
+                             status=400)
 
 
 if __name__ == '__main__':
