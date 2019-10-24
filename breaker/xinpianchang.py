c55a12bba8e6d75003e73e12e1f29ed6c535305b
panoslin <panolin@foxmail.com>
2019-10-24 16:38:12 +0800

    finish breaker xinpianchang

--- breaker/xinpianchang.py
+++ breaker/xinpianchang.py
@@ -3,18 +3,13 @@
 # Created by panos on 2019/7/6
 # IDE: PyCharm
 
-import asyncio
-import aiohttp
 from urllib.parse import unquote
 import html
 import emoji
 import traceback
-from random import choice
 from scrapy import Selector
-import time
-
-from aioVextractor.breaker.base_breaker import BaseBreaker
-from aioVextractor.utils.requests_retry import RequestRetry
+from aioVextractor.breaker import BaseBreaker
+from aioVextractor.utils import RequestRetry
 
 
 class Breaker(BaseBreaker):
@@ -32,27 +27,10 @@ class Breaker(BaseBreaker):
         BaseBreaker.__init__(self, *args, **kwargs)
         self.from_ = "xinpianchang"
 
-    async def breakdown(self, webpage_url, page, session):
-        clips_list = await asyncio.gather(*[self.retrieve_user_paging_api(webpage_url=webpage_url,
-                                                                          page=page,
-                                                                          session=session,
-                                                                          )])
-        for clips in clips_list:
-            results = await self.extract_user_pageing_api(ResText=clips, webpage_url=webpage_url)
-            return results
-
     @RequestRetry
-    async def retrieve_user_paging_api(self, webpage_url, page, session):
+    async def breakdown(self, webpage_url, session, page=1):
         url = "https://www.xinpianchang.com/index.php"
         xinpianchang_user_id = webpage_url.split('?')[0].split('/u')[-1]
-        # headers = {'Origin': "https://www.xinpianchang.com",
-        #            'Accept-Encoding': "gzip, deflate, br",
-        #            'Accept-Language': "zh-CN,zh;q=0.9,en;q=0.8,ko;q=0.7",
-        #            'User-Agent': self.random_ua(),
-        #            'Accept': "*/*",
-        #            'Referer': webpage_url,
-        #            'X-Requested-With': "XMLHttpRequest",
-        #            'cache-control': "no-cache"}
         headers = self.general_headers(user_agent=self.random_ua())
         headers['Origin'] = "https://www.xinpianchang.com"
         headers['Referer'] = webpage_url
@@ -67,7 +45,7 @@ class Breaker(BaseBreaker):
                   "audit": "1",
                   "is_private": "0",
                   "page": page}
-        response_text = await self.request(
+        clips = await self.request(
             url=url,
             session=session,
             headers=headers,
@@ -75,54 +53,41 @@ class Breaker(BaseBreaker):
             method="post",
         )
 
-        # async with session.post(url, headers=headers, params=params) as response:
-        #     response_text = await response.text()
-        return response_text
+        results = await self.extract_user_pageing_api(ResText=clips, webpage_url=webpage_url)
+        return results
 
     async def extract_user_pageing_api(self, ResText, webpage_url):
         try:
             selector = Selector(text=ResText)
         except TypeError:
             return None
+        output = []
+        for article in selector.css("li[data-articleid]"):
+            ele = dict()
+            ele['vid'] = article.css('::attr(data-articleid)').extract_first()
+            ele['webpage_url'] = f"https://www.xinpianchang.com/a{ele['vid']}?from=UserProfile"
+            ele['cover'] = article.css('img[class*="lazy-img"]::attr(_src)').extract_first()
+            ele['upload_ts'] = self.string2timestamp(
+                string=article.css('.video-hover-con p[class*="fs_12"]::text').extract_first(),
+                format='%Y-%m-%d 发布'
+            )
+            # ele['duration'] = self.format_duration(article.css('.duration::text').extract_first())
+            ele['duration'] = self.string2duration(
+                string=article.css('.duration::text').extract_first(),
+                format="%M' %S''"
+            )
+            ele['description'] = self.format_desc(article.css('.desc::text').extract_first())
+            ele['playlist_url'] = webpage_url
+            ele['title'] = self.format_desc(article.css('.video-con-top p::text').extract_first())
+            ele['category'] = self.format_category(article.css('.new-cate .c_b_9 ::text').extract())
+            ele['view_count'] = self.format_count(article.css('.icon-play-volume::text').extract_first())
+            ele['like_count'] = self.format_count(article.css('.icon-like::text').extract_first())
+            ele['role'] = article.css('.user-info .role::text').extract_first()
+            ele['from'] = self.from_
+            output.append(ele)
         else:
-            output = []
-            for article in selector.css("li[data-articleid]"):
-                ele = dict()
-                ele['vid'] = article.css('::attr(data-articleid)').extract_first()
-                ele['webpage_url'] = f"https://www.xinpianchang.com/a{ele['vid']}?from=UserProfile"
-                ele['cover'] = article.css('img[class*="lazy-img"]::attr(_src)').extract_first()
-                ele['upload_ts'] = self.format_upload_ts(
-                    article.css('.video-hover-con p[class*="fs_12"]::text').extract_first())
-                ele['duration'] = self.format_duration(article.css('.duration::text').extract_first())
-                ele['description'] = self.format_desc(article.css('.desc::text').extract_first())
-                ele['playlist_url'] = webpage_url
-                ele['title'] = self.format_desc(article.css('.video-con-top p::text').extract_first())
-                ele['category'] = self.format_category(article.css('.new-cate .c_b_9 ::text').extract())
-                ele['view_count'] = self.format_count(article.css('.icon-play-volume::text').extract_first())
-                ele['like_count'] = self.format_count(article.css('.icon-like::text').extract_first())
-                ele['role'] = article.css('.user-info .role::text').extract_first()
-                ele['from'] = '新片场'
-                output.append(ele)
-            else:
-                has_more = selector.css("li[data-more]::attr(data-more)").extract_first()
-                return output, has_more, {}
-
-    @staticmethod
-    def format_duration(duration_str):
-        """
-        input: "17' 39''"
-        """
-        try:
-            t1 = time.strptime(duration_str, "%M' %S''")
-        except:
-            return None
-        t2 = time.struct_time((1900, 1, 1, 0, 0, 0, 0, 0, -1))
-        try:
-            duration = int(time.mktime(t1) - time.mktime(t2))
-        except Exception:
-            return None
-        else:
-            return duration
+            has_more = selector.css("li[data-more]::attr(data-more)").extract_first()
+            return output, has_more, {}
 
     @staticmethod
     def format_category(category):
@@ -130,28 +95,6 @@ class Breaker(BaseBreaker):
         input: ['\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t系列短视频\t\t\t\t\t\t\t', '\n\t\t\t\t\t\t\t纪录 - 亲情\t\t\t\t\t\t\t']
         """
         return ",".join(list(map(lambda x: x.strip(), category))) if category else None
-
-    @staticmethod
-    def format_upload_ts(upload_ts, detail=0):
-        """
-        input: '2019-05-14 发布'
-        input: '2019-05-19T22:11:39+08:00'
-
-        """
-        if detail == 0:
-            try:
-                return int(time.mktime(time.strptime(upload_ts, '%Y-%m-%d 发布'))) if upload_ts else None
-            except:
-                traceback.print_exc()
-                return None
-        else:
-            try:
-                # upload_ts[:-6] -> '2019-05-19T22:11:39'
-                return int(time.mktime(time.strptime(upload_ts[:-6], '%Y-%m-%dT%H:%M:%S'))) if upload_ts else None
-            except:
-                print(f"upload_ts: {upload_ts}")
-                traceback.print_exc()
-                return None
 
     @staticmethod
     def format_desc(desc):
@@ -173,17 +116,10 @@ class Breaker(BaseBreaker):
 
 
 if __name__ == '__main__':
-    # async def test():
-    #     res = await retrieve_user_paging_api("https://www.xinpianchang.com/u10029931?from=userList")
-    #     async for ele in extract_user_pageing_api(res):
-    #         print(ele)
-
-    async def test():
-        return await breakdown("https://www.xinpianchang.com/u10002513?from=userList", page=1)
-
-
-    loop = asyncio.get_event_loop()
-    _res = loop.run_until_complete(test())
     from pprint import pprint
 
-    pprint(_res)
+    with Breaker() as breaker:
+        res = breaker.sync_breakdown(
+            webpage_url=Breaker.TEST_CASE[0],
+        )
+        pprint(res)
