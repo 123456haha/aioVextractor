183e050c6a4b871dd5cf3496c42020cfbbd7b7eb
panoslin <panolin@foxmail.com>
2019-12-04 18:40:20 +0800

    finish unit testing code

--- test_extractor.py
+++ test_extractor.py
@@ -5,19 +5,16 @@
 """
 RUN ME BEFORE GOING SERIOUS!
 """
-# import sys, os
-#
-# curPath = os.path.abspath(os.path.dirname(__file__))
-# sys.path.append(os.path.dirname(curPath))
-#
-# from pprint import pprint
-# from aioVextractor.extractor import gen_extractor_classes
-# from aioVextractor.breaker import gen_breaker_classes
-# import aiohttp
-# import asyncio
-# from aioVextractor import hybrid_worker
-# from concurrent.futures import ThreadPoolExecutor
-#
+import sys, os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(curPath))
+
+from aioVextractor.extractor import gen_extractor_classes
+from aioVextractor.breaker import gen_breaker_classes
+import aiohttp
+from aioVextractor import hybrid_worker
+
 # async def test(asking=True):
 #     async with aiohttp.ClientSession() as session:
 #         fail_url = set()
@@ -26,24 +23,23 @@ RUN ME BEFORE GOING SERIOUS!
 #                 TEST_CASE = instance.TEST_CASE
 #             except:
 #                 continue
-#             with ThreadPoolExecutor() as executor:
 #
-#                 for sample in TEST_CASE:
-#                     if asking:
-#                         procceed = input(f"proccee webpage_url: {sample}?\n"
-#                                          f"press Enter/n/no to ignore and y/yes to procceed\n"
-#                                          f"enter `pass` to the next extractor TEST_CASE: ")
-#                     else:
-#                         procceed = "yes"
-#                     if procceed in {"y", 'yes'}:
-#                         result = await hybrid_worker(webpage_url=sample, session=session)
-#                         pprint(result)
-#                         if not result:
-#                             fail_url.add(sample)
-#                         print("**************************************************"
-#                               "*************************************************\n")
-#                     elif procceed == "pass":
-#                         break
+#             for sample in TEST_CASE:
+#                 if asking:
+#                     procceed = input(f"proccee webpage_url: {sample}?\n"
+#                                      f"press Enter/n/no to ignore and y/yes to procceed\n"
+#                                      f"enter `pass` to the next extractor TEST_CASE: ")
+#                 else:
+#                     procceed = "yes"
+#                 if procceed in {"y", 'yes'}:
+#                     result = await hybrid_worker(webpage_url=sample, session=session)
+#                     pprint(result)
+#                     if not result:
+#                         fail_url.add(sample)
+#                     print("**************************************************"
+#                           "*************************************************\n")
+#                 elif procceed == "pass":
+#                     break
 #         else:
 #             print(f"fail_url: \n"
 #                   f"{fail_url}")
@@ -51,16 +47,44 @@ RUN ME BEFORE GOING SERIOUS!
 #
 # asyncio.run(test(asking=False))
 
-
+"""
+python -m pytest test_extractor.py --verbose 
+"""
 import pytest
 
-@pytest.mark.asyncio
-async def test_extractor():
-    assert True  == 1
 
-def inc(x):
-    return x + 1
+# @pytest.mark.asyncio
+# async def test_extractor():
+#     async with aiohttp.ClientSession() as session:
+#         for instance in gen_extractor_classes() + gen_breaker_classes():
+#             for sample in instance.TEST_CASE:
+#                 print(sample)
+#                 result = await hybrid_worker(webpage_url=sample, session=session)
+#                 print(result)
+#                 test_benchmark = isinstance(result, list)
+#                 assert test_benchmark
 
 
-def test_answer():
-    assert inc(3) == 5
+# def validate_data(file):
+#     if file == 1:
+#         return True
+#     if file == 2:
+#         return False
+#     if file == 3:
+#         return True
+
+
+class Test_Extractor:
+    @pytest.mark.parametrize(
+        'case', [
+            sample
+            for instance in gen_extractor_classes() + gen_breaker_classes()
+            for sample in instance.TEST_CASE
+        ]
+    )
+    @pytest.mark.asyncio
+    async def test_extractor(self, case):
+        async with aiohttp.ClientSession() as session:
+            result = await hybrid_worker(webpage_url=case, session=session)
+            test_benchmark = isinstance(result, (list, tuple))
+            assert test_benchmark
